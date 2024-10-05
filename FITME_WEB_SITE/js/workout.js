d8189baed6ad40a9111a8ed873e109dc3bcb8d91
RandDabbour <Randdabbourr@gmail.com>
2024-10-05 16:36:57 -0700

    last

--- FITME_WEB_SITE/js/workout.js
+++ FITME_WEB_SITE/js/workout.js
@@ -1,5 +1,5 @@
 
-fetch('http://127.0.0.1:5500/FitME_PROJECT_JAVASCRIPT-main/FITME_WEB_SITE/js/workout.json')
+fetch('http://127.0.0.1:5502/FITME_WEB_SITE/js/workout.json')
 .then(response => {
     if (!response.ok) {
         throw new Error('Network response was not ok');
